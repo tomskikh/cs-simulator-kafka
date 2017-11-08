34fe3e1656d01a3996514d05c6d2007be9503abd
Vladislav Medvedev <medvedev_vv@bw-sw.com>
2017-11-08 16:49:43 +0700

    CSSK-1 KAFKA_CONTAINER parameter was added to integration tests

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,4 +1,4 @@
-if [ $# -ne 4 ]; then
+if [ $# -ne 5 ]; then
     echo "Please run this script with four parameters (KAFKA_HOST, KAFKA_PORT, KAFKA_TOPIC, SIMULATOR_PORT)"
     exit 1;
 fi
@@ -7,10 +7,11 @@ KAFKA_HOST=$1
 KAFKA_PORT=$2
 KAFKA_TOPIC=$3
 SIMULATOR_PORT=$4
+KAFKA_CONTAINER=$5
 
 curl --fail http://localhost:${SIMULATOR_PORT}/client/ --retry 40 --retry-delay 30
 
-test $(docker exec -it ./bin/kafka-run-class.sh kafka.tools.GetOffsetShell \
+test $(docker exec -it $KAFKA_CONTAINER ./bin/kafka-run-class.sh kafka.tools.GetOffsetShell \
         --broker-list $KAFKA_HOST: $KAFKA_PORT \
         --topic $KAFKA_TOPIC --time -1 --offsets 1 \
         | awk -F ":" '{sum += $3} END {print sum}' \
